f6ee19f5320fdc922ced2f403b4331e5b2a76cfb
githufei <460284524@qq.com>
2017-08-11 10:22:51 +0800

    1

---
+++ app/containers/Home/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by z on 2017/8/7.
+ */
+import React,{Component} from 'react';
+import Slider from "../../components/Slider/index";
+import List from "../../components/List/index";
+import './index.less'
+import Content from "../../components/content/Content";
+
+export default class Home extends Component{
+    render(){
+        console.log(this.props);//Object {match: Object, location: Object, history: Object, staticContext:
+        return(
+            <div>
+                <div className="row header" style={{backgroundColor:"#eee",margin:0,padding:0}}>
+                    <div className="col-xs-12" style={{margin:10}}>
+                        <div className="col-xs-10">
+                            <input className="form-control" style={{borderRadius:17}} type="text" placeholder="搜索你想要的商品吧"/>
+                        </div>
+                        <div className="col-xs-2">
+                            <span className="search">搜索</span>
+                        </div>
+                    </div>
+                </div>
+                    <Slider/>
+                    <List/>
+            </div>
+
+        )
+    }
+}
---
+++ app/containers/List/index.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+import {HashRouter as Router,Route,Link} from 'react-router-dom';
+import Hot from './components/Hot/index';
+import NorthChina from './components/NorthChina/index';
+import Northeast from './components/Northeast/index';
+import Other from './components/Other/index';
+import SouthChina from './components/SouthChina/index';
+import Header from "../../components/Public/Header";
+
+export default class List extends Component{
+    constructor(){
+        super();
+        this.state={title:"吃货天堂"}
+    }
+    render(){
+        return(
+            <Router>
+                <div className="eat">
+                    <Header title={this.state.title}/>
+                    <ul className="allFood">
+                        <li><Link to="/list">热销特产</Link></li>
+                        <li><Link to="/list/northChina" >东北特产</Link></li>
+                        <li><Link to="/list/northeast" >华北特产</Link></li>
+                        <li><Link to="/list/southChina">华南特产</Link></li>
+                        <li><Link to="/list/other">其他地区</Link></li>
+                    </ul>
+                    <Route exact path="/list" component={Hot}/>
+                    <Route path="/list/northChina" component={NorthChina}/>
+                    <Route path="/list/northeast" component={Northeast}/>
+                    <Route path="/list/other" component={Other}/>
+                    <Route path="/list/southChina" component={SouthChina}/>
+                </div>
+            </Router>
+        )
+    }
+}
+import './index.less';
